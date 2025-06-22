f670ab98d932f29f2011fa68ca693ff6fd14fb85
Ivan-Dzhyga06 <ta4erka72@gmail.com>
2025-06-22 21:38:06 +0200

    DZ

--- DZ1.py
+++ DZ1.py
@@ -1,25 +1,20 @@
 import heapq
 
 def min_total_cost(cables):
-    # Перетворюємо список кабелів у мін-купу
     heapq.heapify(cables)
     
     total_cost = 0
     
     while len(cables) > 1:
-        # Виймаємо два найкоротші кабелі
         first = heapq.heappop(cables)
         second = heapq.heappop(cables)
         
-        # Об'єднуємо їх
         cost = first + second
         total_cost += cost
         
-        # Додаємо новий кабель назад у купу
         heapq.heappush(cables, cost)
     
     return total_cost
 
-# Приклад використання
 cables = [4, 3, 2, 6]
 print(f"Мінімальні витрати на з'єднання: {min_total_cost(cables)}")
